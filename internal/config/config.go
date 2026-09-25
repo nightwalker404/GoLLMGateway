91df4ecad7554eb24b3ef3e45a9fb42641aac0e3
nightwalker404 <nobodyisherefornow1889@gmail.com>
2026-09-25 16:53:45 +0330

    the main config oot the project

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"time"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type OllamaConfig struct {
+	BaseUrl string        `env:"OLLAMA_BASE_URL" envDefault:"http://localhost:11434"`
+	Timeout time.Duration `env:"OLLAMA_TIMEOUT" envDefault:"60s"`
+}
+
+type VllmConfig struct {
+	BaseUrl string        `env:"VLLM_BASE_URL" envDefault:"http://localhost:8000"`
+	Timeout time.Duration `env:"VLLM_TIMEOUT" envDefault:"60s"`
+}
+
+type config struct {
+	ServerAddr      string `env:"SERVER_ADDR" envDefault:"8080"`
+	DefaultProvider string `env:"DEFAULT_PROVIDER" envDefault:"ollama"`
+
+	Ollama OllamaConfig
+	Vllm   VllmConfig
+}
+
+func load() (config, error) {
+	cfg := config{}
+	if err := env.FieldParams(&cfg); err != nil {
+		return config{}, err
+	}
+	return cfg, nil
+}
